46fefe83623e2a6e6d3fc081b24039c5499c60df
zch <1351572481@qq.com>
2021-08-30 09:21:23 +0800

    初始化

---
+++ src/main/java/com/example/bootlearning/BootLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bootlearning/BootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
